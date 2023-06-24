0e65bdbbb37a3e6ebb3dd5e6e402847b000b5d6e
hoheesu <hoheesu@naver.com>
2023-06-24 18:04:12 +0900

    Feat: Change Icon to image

--- src/components/UserIcon.tsx
+++ src/components/UserIcon.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { Icon } from "@iconify/react";
 import styled from "styled-components";
 import { mixins } from "../styles/theme";
 
 function UserIcon() {
   return (
     <FlexBox>
-      {/* <User icon="solar:user-bold" /> */}
-      <h2>윤준수</h2>
+      <IconBox>
+        <UserImg src="src/assets/userImg.png" alt="user icon" />
+      </IconBox>
+      <UserName>윤준수</UserName>
       <ClickBox>
         <h3>포트폴리오를 보시려면 클릭해주세요</h3>
       </ClickBox>
@@ -18,7 +19,12 @@ const FlexBox = styled.div`
   ${mixins.centerFlex};
   flex-direction: column;
   width: max-content;
-  gap: 1em;
+  gap: 2em;
+`;
+const UserName = styled.h2`
+  font-size: 2em;
+  color: var(--color-box);
+  text-shadow: 1px 1px 1px #333;
 `;
 const ClickBox = styled.div`
   ${mixins.centerFlex};
@@ -27,7 +33,13 @@ const ClickBox = styled.div`
   height: 2em;
   border-radius: 1em;
 `;
-const User = styled(Icon)`
+const IconBox = styled.div`
+  ${mixins.centerFlex};
+  width: max-content;
+  border-radius: 50%;
+  background-color: var(--color-box);
+`;
+const UserImg = styled.img`
   color: var(--color-icon);
   width: 10em;
   height: 10em;
