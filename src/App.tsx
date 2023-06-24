c47dfac7ce748b72593d3504b66e52b0ca0d4159
hoheesu <hoheesu@naver.com>
2023-06-24 17:47:28 +0900

    Feat: Lender Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import "./App.css";
-import { Reset } from "styled-reset";
+import styled from "styled-components";
+import GlobalStyle from "./styles/globalstyle.tsx";
+import UserIcon from "./components/UserIcon.tsx";
+import { mixins } from "./styles/theme";
 
 function App() {
   return (
     <>
-      <Reset />
-      <div>Hello</div>
+      <GlobalStyle />
+      <MainBg>
+        <UserIcon />
+      </MainBg>
     </>
   );
 }
-
+const backgroundImg = "src/assets/portfolioBg.jpg";
+const MainBg = styled.div`
+  ${mixins.centerFlex};
+  background-image: url(${backgroundImg});
+  width: 100vw;
+  height: 100vh;
+`;
 export default App;
---
+++ src/components/UserIcon.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Icon } from "@iconify/react";
+import styled from "styled-components";
+import { mixins } from "../styles/theme";
+
+function UserIcon() {
+  return (
+    <FlexBox>
+      <User icon="solar:user-bold" />
+      <h2>윤준수</h2>
+      <ClickBox>
+        <h3>포트폴리오를 보시려면 클릭해주세요</h3>
+      </ClickBox>
+    </FlexBox>
+  );
+}
+const FlexBox = styled.div`
+  ${mixins.centerFlex};
+  flex-direction: column;
+  width: max-content;
+  gap: 1em;
+`;
+const ClickBox = styled.div`
+  ${mixins.centerFlex};
+  background-color: var(--color-box);
+  width: 20em;
+  height: 2em;
+  border-radius: 1em;
+`;
+const User = styled(Icon)`
+  color: var(--color-icon);
+  width: 10em;
+  height: 10em;
+`;
+
+export default UserIcon;
