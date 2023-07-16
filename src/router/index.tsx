953ec4d317b40a3a67b097885ca8bb796a4aa0a3
hoheesu <hoheesu@naver.com>
2023-07-16 22:54:23 +0900

    Refactor: 사용하지 않는 hook 정리

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import App from "../App.tsx";
 import MainPortfolio from "../page/MainPortfolio.tsx";
 
